9c637d408426f3188ebca2e49a2b4d0bea9466b3
hikaru-stannum <h.suzue@infomart.co.jp>
2026-05-20 14:44:54 +0900

    ユーサー認証の実装

---
+++ src/main/java/in/tech_camp/chat_app/custom_user/CustomUserDetail.java
@@ -0,0 +1,60 @@
+package in.tech_camp.chat_app.custom_user;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import in.tech_camp.chat_app.entity.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CustomUserDetail implements UserDetails{
+  private final UserEntity user;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities(){
+        return Collections.emptyList();
+  }
+
+  @Override
+  public String getPassword(){
+    return user.getPassword();
+  }
+
+  @Override
+  public String getUsername(){
+    return user.getEmail();
+  }
+
+  public Integer getId(){
+    return user.getId();
+  }
+
+  public String getName(){
+    return user.getName();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+      return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+      return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+      return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+      return true;
+  }
+}
