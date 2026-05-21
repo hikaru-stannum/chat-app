c34bf33531b33147cde318b253e1d750a0ef47fc
hikaru-stannum <h.suzue@infomart.co.jp>
2026-05-21 14:09:08 +0900

    チャットルーム作成機能の実装

--- src/main/java/in/tech_camp/chat_app/form/RoomForm.java
+++ src/main/java/in/tech_camp/chat_app/form/RoomForm.java
@@ -2,11 +2,13 @@ package in.tech_camp.chat_app.form;
 
 import java.util.List;
 
+import in.tech_camp.chat_app.repository.validation.ValidationPriority1;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RoomForm {
-  private Integer id;
+  @NotBlank(message = "Room Name can't be blank", groups = ValidationPriority1.class)
   private String name;
   private List<Integer> memberIds;
 }
