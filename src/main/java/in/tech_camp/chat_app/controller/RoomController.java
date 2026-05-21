e61700034781a578bd96c48d05de013c6bada4f0
hikaru-stannum <h.suzue@infomart.co.jp>
2026-05-21 11:29:50 +0900

    プルダウンの実装

--- src/main/java/in/tech_camp/chat_app/controller/RoomController.java
+++ src/main/java/in/tech_camp/chat_app/controller/RoomController.java
@@ -1,9 +1,15 @@
 package in.tech_camp.chat_app.controller;
 
+import java.util.List;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+import in.tech_camp.chat_app.custom_user.CustomUserDetail;
+import in.tech_camp.chat_app.entity.UserEntity;
 import in.tech_camp.chat_app.form.RoomForm;
+import in.tech_camp.chat_app.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,10 +18,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @AllArgsConstructor
 public class RoomController {
+  private UserRepository userRepository;
 
   @GetMapping("/rooms/new")
-  public String showRoomNew(Model model) {
-     model.addAttribute("roomForm", new RoomForm());
+  public String showRoomNew(@AuthenticationPrincipal CustomUserDetail currentUser, Model model) {
+    List<UserEntity> users = userRepository.findAllExcept(currentUser.getId());
+    model.addAttribute("users",users);
+    model.addAttribute("roomForm", new RoomForm());
       return "rooms/new";
   }
 }
--- src/main/java/in/tech_camp/chat_app/form/RoomForm.java
+++ src/main/java/in/tech_camp/chat_app/form/RoomForm.java
@@ -1,9 +1,12 @@
 package in.tech_camp.chat_app.form;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class RoomForm {
   private Integer id;
   private String name;
+  private List<Integer> memberIds;
 }
